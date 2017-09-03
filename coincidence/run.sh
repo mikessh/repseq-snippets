f367794bfb1f116817bdbdd4445ea8fe6576db3e
mikessh <mikhail.shugay@gmail.com>
2017-09-03 18:07:04 +0300

    WIP

--- coincidence/run.sh
+++ coincidence/run.sh
@@ -1,13 +1,13 @@
 rm PublicCoincidence.class 
 javac PublicCoincidence.java
 
+# Fetch symlinked
 cd ../samples
 SS=`ls [HK]*.txt | paste -sd "," -`  # all samples
-
-cd ../coincidence/
+cd ..
 
 # Other parameters:
 # freq threshold for publics, P&log10 odds threshold for association - comma-separated
 # file with pooled clonotypes, incl incidence count
 
-java -Xmx100G -cp . PublicCoincidence 0.05,0.001,0.3 ../hip.pool.txt "$SS" hip_assoc
+java -Xmx100G -cp coincidence/ PublicCoincidence 0.05,0.001,0.3 hip.pool.txt "$SS" hip_assoc
